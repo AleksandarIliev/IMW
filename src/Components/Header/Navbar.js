4587f0b16f39ae79704a428c1fa5f6fb4ea3689b
Aleksandar Iliev <aleksandar.m.iliev@abv.bg>
2024-01-01 16:23:07 +0200

    Add responsive navBtn and for them

--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -1,6 +1,10 @@
 import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
+    const handleNavBtn = () => {
+        
+    }
+
     return (
         <div className="navbar">
             <NavLink to="/">IMW Ltd.</NavLink>
@@ -8,6 +12,11 @@ export const Navbar = () => {
             <NavLink to="/team">Team</NavLink>
             <NavLink to="/products">Products</NavLink>
             <NavLink to="/contact">Contact</NavLink>
+            <div className="navBtn" onClick={handleNavBtn}>
+                <div className="lineOne"></div>
+                <div className="lineTwo"></div>
+                <div className="lineThree"></div>
+            </div>
         </div>
     );
 }
