bec4611f2bd9c55908ac5a7d4d1045ac828008d8
Aleksandar Iliev <aleksandar.m.iliev@abv.bg>
2024-01-20 20:55:17 +0200

    Create a language folder with current languages

---
+++ src/Languages/Languages.js
@@ -0,0 +1,11 @@
+import bg from "./bg.json";
+import de from "./de.json";
+import en from "./en.json";
+
+export const langList = { bg, de, en };
+
+export const langOption = {
+    bg: "Bulgarien",
+    de: "Deutsch",
+    en: "English",
+};
