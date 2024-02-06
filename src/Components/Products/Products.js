def7760ac0750f2b674ea5ed9c9cd44f419b59f6
Aleksandar Iliev <aleksandar.m.iliev@abv.bg>
2024-02-06 21:36:52 +0200

    Add Products.css with little style and fix Language and Products

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { LanguageContext } from "../Context/LanguageContext";
-import { Item } from "./Item"
+import { Item } from "./Item";
+import styles from "./Products.module.css"
 
 export const Products = () => {
     const dictionary = useContext(LanguageContext);
@@ -8,7 +9,7 @@ export const Products = () => {
 
     return (
         <div>
-            <ul className="product"> {productsObj.map(x => <li className="liStyle" key={x.id}><Item {...x}/></li>)}</ul>
+            <ul className={styles.product}> {productsObj.map(x => <li className={styles.liStyle} key={x.id}><Item {...x}/></li>)}</ul>
         </div>
     )
 }
