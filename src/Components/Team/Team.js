75942a234797a9050f56c6371d5c2285ef519b9b
Aleksandar Iliev <aleksandar.m.iliev@abv.bg>
2024-02-25 20:08:39 +0200

    Add content in Team component

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -1,24 +1,39 @@
 import { LanguageContext } from "../Context/LanguageContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Collapsible } from "./Collapsible";
 import "./Team.module.css";
 
 export const Team = () => {
     const { dictionary } = useContext(LanguageContext);
+    const [index, setIndex] = useState(0);
+    const photoLength = dictionary.contentHome.length - 1;
+    console.log(photoLength);
+
+    const prev = () => {
+        setIndex = index - 1;
+    }
+
+    const next = () => {
+        setIndex = index + 1;
+    }
+
+    // const change = () => {
+    //     if (index = photoLength) {
+    //         setIndex = 0;
+    //     } else {
+    //         setIndex = photoLength;
+    //     }
+    // }
 
     return (
         <div>
             <Collapsible label="Certificates">
                 <div className="container">
-                    <img className="mySlides" src="img_nature_wide.jpg" alt="" />
-                    <img className="mySlides" src="img_snow_wide.jpg" alt="" />
-                    <img className="mySlides" src="img_mountains_wide.jpg" alt="" />
+                    <img className="mySlides" src={dictionary.contentHome[index]} alt="" />
                     <div className="bottommiddle">
-                        <div className="w3-left w3-hover-text-khaki" onClick="plusDivs(-1)">&#10094;</div>
-                        <div className="w3-right w3-hover-text-khaki" onClick="plusDivs(1)">&#10095;</div>
+                        <div className="w3-left w3-hover-text-khaki" onClick={prev}>&#10094;</div>
+                        <div className="w3-right w3-hover-text-khaki" onClick={next}>&#10095;</div>
                         <span className="w3-badge demo w3-border w3-transparent w3-hover-white" onClick="currentDiv(1)"></span>
-                        <span className="w3-badge demo w3-border w3-transparent w3-hover-white" onClick="currentDiv(2)"></span>
-                        <span className="w3-badge demo w3-border w3-transparent w3-hover-white" onClick="currentDiv(3)"></span>
                     </div>
                 </div>
             </Collapsible>
