22553a8f90b68dd05da693bd9429bcd270b67de3
Aleksandar Iliev <aleksandar.m.iliev@abv.bg>
2024-03-21 21:12:42 +0200

    Fix to be visible LangBtn

--- src/Components/LangBtn/LangSelector.js
+++ src/Components/LangBtn/LangSelector.js
@@ -1,31 +1,3 @@
-// import { useContext, useState } from "react"
-// import { LangContext } from "../Context/LangContext"
-
-// export const LangBtn = () => {
-//     const [isClick, setIsClick]= useState(false);
-//     const { lang, setLang } = useContext(LangContext)
-//     const changeLang = () => {
-//         setIsClick(!isClick)
-//     }
-    
-//     return (
-//         <span className="language">
-//             <span>
-//                 <button onClick={changeLang}>
-//                     {lang}
-//                 </button>
-//             </span>
-//             {isClick ?
-//                 <div>
-//                     <div><button onClick={() => setLang('Bg')}>Bg</button></div>
-//                     <div><button onClick={() => setLang('En')}>En</button></div>
-//                     <div><button onClick={() => setLang('De')}>De</button></div>
-//                 </div> : ''
-//             }
-//         </span>
-//     )
-// }
-
 import { useContext } from "react";
 
 import { langOption } from "../../Languages/Languages";
