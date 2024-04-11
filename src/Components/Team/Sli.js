a91d42154a9f3a6fda380218813b421b2d63aef5
Aleksandar Iliev <aleksandar.m.iliev@abv.bg>
2024-04-11 21:43:00 +0300

    Import React-Image-Gallery

--- src/Components/Team/Sli.js
+++ src/Components/Team/Sli.js
@@ -1,7 +1,8 @@
 import "./Sli.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { useContext, useState, useEffect } from "react";
 import { LanguageContext } from "../Context/LanguageContext";
+import ImageGallery from "react-image-gallery";
 
 export const Sli = () => {
     const { dictionary } = useContext(LanguageContext);
@@ -14,7 +15,7 @@ export const Sli = () => {
         } else if (index > lastIndex) {
             setIndex(0);
         }
-    }, [index, dictionary.contentTeam])
+    }, [index, dictionary.contentTeam[0].certificates])
 
     function plusSlide(n) {
         showSlide(setIndex += n)
@@ -35,49 +36,65 @@ export const Sli = () => {
     }
 
     return (
-        <div>
-            {/* <!-- Container for the image gallery-- > */}
-            <div className="container">
+        // <div>
+        //     {/* <!-- Container for the image gallery-- > */}
+        //     <div className="container">
 
-                {/* <!-- Full-width images with number text --> */}
-                <div className="mySlides">
-                    {dictionary.contentTeam[0].certificates.map((slide, slideIndex) => {
-                        const { id, image, description } = slide;
-                        let position = "nextSlide";
-                        if (slideIndex === index) {
-                            position = "activeSlide";
-                        }
-                        if (
-                            slideIndex === index - 1 ||
-                            (index === 0 && slideIndex === dictionary.contentTeam.length - 1)
-                        ) {
-                            position = "lastSlide";
-                        }
-                        return (
-                            <>
-                                <div className="numbertext">{id} / {slide.length - 1}</div>
-                                <img className="img" src={image} alt={description} />
-                            </>
-                        )
-                    })}
+        //         {/* <!-- Full-width images with number text --> */}
+        //         <div className="mySlides">
+        //             {dictionary.contentTeam[0].certificates.map((slide, slideIndex) => {
+        //                 const { id, image, description } = slide;
+        //                 let position = "nextSlide";
+        //                 if (slideIndex === index) {
+        //                     position = "activeSlide";
+        //                 }
+        //                 if (
+        //                     slideIndex === index - 1 ||
+        //                     (index === 0 && slideIndex === dictionary.contentTeam.length - 1)
+        //                 ) {
+        //                     position = "lastSlide";
+        //                 }
+        //                 return (
+        //                     <>
+        //                         <div className="numbertext">{id} / {slide.length - 1}</div>
+        //                         <img className="img" src={image} alt={description} />
+        //                     </>
+        //                 )
+        //             })}
+        //         </div>
+
+        //         {/* <!-- Next and previous buttons --> */}
+        //         <Link className="prev" onClick={() => setIndex(index - 1)}>&#10094;</Link>
+        //         <Link className="next" onClick={() => setIndex(index + 1)}>&#10095;</Link>
+
+        //         {/* <!-- Image text --> */}
+        //         <div className="caption-container">
+        //             <p id="caption"></p>
+        //         </div>
+
+        //         {/* <!-- Thumbnail images --> */}
+        //         <div className="row">
+        //             <div className="column">
+        //                 <img className="demo cursor" src="img_woods.jpg" onClick="currentSlide(1)" alt="The Woods" />
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+
+        <div className="app">
+            <header>
+                <div className="header-wrapper">
+                    <h1>{dictionary.contentTeam.description}</h1>
                 </div>
+            </header>
+            {dictionary.contentTeam[0].certificates.map((content, contentIndex) => {
+                const { id, image, description } = content;
 
-                {/* <!-- Next and previous buttons --> */}
-                <Link className="prev" onClick={() => setIndex(index - 1)}>&#10094;</Link>
-                <Link className="next" onClick={() => setIndex(index + 1)}>&#10095;</Link>
-
-                {/* <!-- Image text --> */}
-                <div className="caption-container">
-                    <p id="caption"></p>
+                <div className="image-gallery-wrapper" key={id}>
+                    <ImageGallery items={image} autoPlay={true} showThumbnails={true} />
+                    <spam>{description}</spam>
                 </div>
-
-                {/* <!-- Thumbnail images --> */}
-                <div className="row">
-                    <div className="column">
-                        <img className="demo cursor" src="img_woods.jpg" onClick="currentSlide(1)" alt="The Woods" />
-                    </div>
-                </div>
-            </div>
+            })}
         </div>
     )
 }
