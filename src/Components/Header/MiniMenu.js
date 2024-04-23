6efc00d3bc868845640475788a29722156f20819
Aleksandar Iliev <aleksandar.m.iliev@abv.bg>
2024-04-23 22:13:28 +0300

    Try to separete Navbar logic into two separate component

---
+++ src/Components/Header/MiniMenu.js
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom";
+import { useContext, useState } from "react";
+import { LanguageContext } from "../Context/LanguageContext";
+import { RiAccountCircleLine } from "react-icons/ri";
+
+export const MiniMenu = () => {
+    return (
+
+    )
+}
